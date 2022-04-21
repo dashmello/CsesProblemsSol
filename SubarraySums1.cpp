d307b3c079f3c4ec59c65dcabf288737a0dc6d8a
Dương Đức Nghĩa <nghia.duong222@hcmut.edu.vn>
2022-04-21 17:30:08 +0700

    1st

---
+++ SubarraySums1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<iomanip>
+#include<algorithm>
+#include<iterator>
+#include<stdlib.h>
+#include<math.h>
+#include<numeric>
+#include<set>
+#include<array>
+#include<map>
+#include<queue>
+#include<stack>
+using namespace std;
+#define ll long long
+
+
+int main(){
+    int n;
+    cin>>n;
+    
+}
