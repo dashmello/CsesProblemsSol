e27a4ab6a48132c25e77159b2bf1b73f9865220d
Dương Đức Nghĩa <nghia.duong222@hcmut.edu.vn>
2022-04-22 23:05:22 +0700

    2nd

--- SubarraySums2.cpp
+++ SubarraySums2.cpp
@@ -21,26 +21,21 @@ int main(){
     cin>>n;
     int sum;
     cin >>sum;
-    vector<ll>v(n+1);
-    v[0] = 0;
-    for (size_t i = 1; i <= n; i++)
+    vector<ll>v(n);
+    for (size_t i = 0; i < n; i++)
     {
         cin >> v[i];
-    }
-    vector<ll>v1(n + 1);
-    v1[0] = 0;
-    for (size_t i = 1; i <= n; i++)
-    {
-        v1[i] = v1[i-1]+ v[i];
     }
     map<ll,int> mp;
     mp[0] ++;
     ll count = 0;
-    for (size_t i = 1; i <= n; i++)
+    ll t = 0;
+    for (size_t i = 0; i < n; i++)
     {
-        ll x = v1[i] - sum;
-        count += mp[x];
-        mp[v1[i]]++;
+        t+= v[i];
+        count+=mp[t - sum];
+        mp[t]++;
+        
     }
     cout << count;
     
