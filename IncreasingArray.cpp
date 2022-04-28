bf1827f9f169af56a199889d15efb0927193b6fe
Dương Đức Nghĩa <nghia.duong222@hcmut.edu.vn>
2022-04-28 15:33:56 +0700

    2rd

--- IncreasingArray.cpp
+++ IncreasingArray.cpp
@@ -1,36 +1,22 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include<iomanip>
 #include<algorithm>
-#include<iterator>
-#include<stdlib.h>
 #include<math.h>
-#include<numeric>
-#include<set>
-#include<array>
-#include<map>
-#include<queue>
-#include<stack>
 using namespace std;
 #define ll long long
-ll ar[200000];
-int main(){
-    int n;
-    cin >> n;
-    
-    for (size_t i = 0; i < n; i++)
-    {
-        cin >> ar[i];
-    }
-    ll m = ar[0];
-    ll c = 0;
-    for (size_t i = 1; i < n; i++)
-    {
-        m = std::max(m,ar[i]);
-        c += m-ar[i];
-    }
-    cout << c;
-    
-    
+int main() {
+	ll n;
+	cin >> n;
+	ll ans = 0;
+	ll xx; cin >> xx;
+	ll max = xx;
+	for (ll i = 0; i < n - 1; i++) {
+		ll x;
+		cin >> x;
+		max = std::max(max, x);
+		ans += (max - x);
+	}
+	cout << ans;
 }
+ 
